ee096f39a8810da8877822fd87bc29c7cafeb3f0
Carlos Aguilar <hey@carlos-aguilar.com>
2025-10-06 01:25:50 +0200

    chore: add box function

--- lib/box.lua
+++ lib/box.lua
@@ -1,6 +1,11 @@
 function box(cx, cy, cz, sx, sy, sz)
   local d =
     max(max(abs(x - cx) - sx / 2, abs(y - cy) - sy / 2), abs(z - cz) - sz / 2)
-
   return d <= 0
+end
+
+function box(bx, by, bz, width, height, depth)
+  return inrange(x, bx, bx + width - 1)
+    and inrange(y, by, by + height - 1)
+    and inrange(z, bz, bz + depth - 1)
 end
